aa45313337526fdd93c3eb69e16f0f9f9232932d
Asif Azimi <asifazimi620@gmail.com>
2022-06-30 14:17:39 +0430

    SingleCocktail page added

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,10 +1,113 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
+import Loading from "../components/Loading";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SignleCocktail = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    const getData = async () => {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+          const newCocktail = {
+            name,
+            info,
+            image,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!cocktail) {
+    return <h2 className="section-title">no cocktail matched!</h2>;
+  }
+
+  const { name, info, image, glass, instructions, category, ingredients } =
+    cocktail;
   return (
-    <>
-      <h1>SignleCocktail page</h1>
-    </>
+    <section className="section cocktail-section">
+      <Link to="/" className="btn btn-primary ">
+        back to home
+      </Link>
+      <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">name: </span>
+            {name}
+          </p>
+          <p>
+            <span className="drink-data">info: </span>
+            {info}
+          </p>
+          <p>
+            <span className="drink-data">category: </span>
+            {category}
+          </p>
+          <p>
+            <span className="drink-data">glass: </span>
+            {glass}
+          </p>
+          <p>
+            <span className="drink-data">instructions: </span>
+            {instructions}
+          </p>
+          <p>
+            <span className="drink-data">ingredients :</span>
+            {ingredients.map((item, index) => {
+              return item ? <span key={index}> {item}</span> : null;
+            })}
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
